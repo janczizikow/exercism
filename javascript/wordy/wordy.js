f6e3a25e375086c0369bf9c851ba370ec49247ec
Jan Czizikow <jan.czizikow@gmail.com>
2020-10-20 02:19:22 +0200

    solve wordy in js

--- javascript/wordy/wordy.js
+++ javascript/wordy/wordy.js
@@ -1,9 +1,58 @@
+export class ArgumentError extends Error {}
+
 export class WordProblem {
   constructor(question) {
     this.question = question;
   }
 
+  sum(x, y) {
+    return x + y;
+  }
+
+  substract(x, y) {
+    return x - y;
+  }
+
+  multiply(x, y) {
+    return x * y;
+  }
+
+  divide(x, y) {
+    return x / y;
+  }
+
+  operation(operator) {
+    if (operator.match(/(add|plus)/)) {
+      return this.sum;
+    } else if (operator.match(/(minus)/)) {
+      return this.substract;
+    } else if (operator.match(/(multiply?|multiplied)/)) {
+      return this.multiply;
+    } else if (operator.match(/(divided?)/)) {
+      return this.divide;
+    } else {
+      throw new Error(`unrecognized operator: "${operator}"`);
+    }
+  }
+
   answer() {
-    return 0;
+    const numbers = this.question.match(/-?\d+/g)?.map(Number) ?? [];
+    const operators = this.question.match(
+      /(add|plus|minus|multiply?|multiplied|divided?)/g
+    );
+
+    if (numbers.length < 2 || !operators.length) {
+      throw new ArgumentError(
+        "Must provide at least 2 numbers and one recognized operation"
+      );
+    }
+
+    const [start, ...rest] = numbers;
+
+    return rest.reduce((acc, cur) => {
+      const head = operators.shift();
+      const operation = this.operation(head);
+      return operation(acc, cur);
+    }, start);
   }
 }
--- javascript/wordy/wordy.spec.js
+++ javascript/wordy/wordy.spec.js
@@ -6,79 +6,79 @@ describe('Word Problem', () => {
     expect(new WordProblem(question).answer()).toEqual(2);
   });
 
-  test('add 2', () => {
+  xtest('add 2', () => {
     const question = 'What is 53 plus 2?';
     expect(new WordProblem(question).answer()).toEqual(55);
   });
 
-  test('add negative numbers', () => {
+  xtest('add negative numbers', () => {
     const question = 'What is -1 plus -10?';
     expect(new WordProblem(question).answer()).toEqual(-11);
   });
 
-  test('add more digits', () => {
+  xtest('add more digits', () => {
     const question = 'What is 123 plus 45678?';
     expect(new WordProblem(question).answer()).toEqual(45801);
   });
 
-  test('subtract', () => {
+  xtest('subtract', () => {
     const question = 'What is 4 minus -12?';
     expect(new WordProblem(question).answer()).toEqual(16);
   });
 
-  test('multiply', () => {
+  xtest('multiply', () => {
     const question = 'What is -3 multiplied by 25?';
     expect(new WordProblem(question).answer()).toEqual(-75);
   });
 
-  test('divide', () => {
+  xtest('divide', () => {
     const question = 'What is 33 divided by -3?';
     expect(new WordProblem(question).answer()).toEqual(-11);
   });
 
-  test('add twice', () => {
+  xtest('add twice', () => {
     const question = 'What is 1 plus 1 plus 1?';
     expect(new WordProblem(question).answer()).toEqual(3);
   });
 
-  test('add then subtract', () => {
+  xtest('add then subtract', () => {
     const question = 'What is 1 plus 5 minus -2?';
     expect(new WordProblem(question).answer()).toEqual(8);
   });
 
-  test('subtract twice', () => {
+  xtest('subtract twice', () => {
     const question = 'What is 20 minus 4 minus 13?';
     expect(new WordProblem(question).answer()).toEqual(3);
   });
 
-  test('subtract then add', () => {
+  xtest('subtract then add', () => {
     const question = 'What is 17 minus 6 plus 3?';
     expect(new WordProblem(question).answer()).toEqual(14);
   });
 
-  test('multiply twice', () => {
+  xtest('multiply twice', () => {
     const question = 'What is 2 multiplied by -2 multiplied by 3?';
     expect(new WordProblem(question).answer()).toEqual(-12);
   });
 
-  test('add then multiply', () => {
+  xtest('add then multiply', () => {
     const question = 'What is -3 plus 7 multiplied by -2?';
     expect(new WordProblem(question).answer()).toEqual(-8);
   });
 
-  test('divide twice', () => {
+  xtest('divide twice', () => {
     const question = 'What is -12 divided by 2 divided by -3?';
     expect(new WordProblem(question).answer()).toEqual(2);
   });
 
-  test('too advanced', () => {
+  xtest('too advanced', () => {
     const question = 'What is 53 cubed?';
     const problem = new WordProblem(question);
 
     expect(problem.answer.bind(problem)).toThrow(ArgumentError);
   });
 
-  test('irrelevant', () => {
+  xtest('irrelevant', () => {
     const question = 'Who is the president of the United States?';
     const problem = new WordProblem(question);
 
