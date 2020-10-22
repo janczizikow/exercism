20511756b3116e61f40e8c19cb9ce1a115a3989c
Jan Czizikow <jan.czizikow@gmail.com>
2020-10-22 16:46:51 +0200

    solve run length encoding in js

--- javascript/run-length-encoding/run-length-encoding.js
+++ javascript/run-length-encoding/run-length-encoding.js
@@ -1,17 +1,29 @@
-export const encode = message => {
-  return "";
+export const encode = (message) => {
+  const seq = [];
+  let lastChar = null;
+
+  for (let i = 0; i < message.length; i++) {
+    if (message[i] === lastChar) {
+      seq[seq.length - 1].push(message[i]);
+    } else {
+      seq.push([message[i]]);
+    }
+
+    lastChar = message[i];
+  }
+
+  return seq.reduce(
+    (acc, cur) => (acc += cur.length > 1 ? `${cur.length}${cur[0]}` : cur[0]),
+    ""
+  );
 };
 
-export const decode = message => {
-  // "12WB12W3B24WB"
-  // let copy = message;
-  let output = "";
-  // while (copy.length) {
-  //   const num = parseInt(copy);
-  //   const letter = copy.match(/[a-z]/i);
-  //   output += num ? letter[0].repeat(num) : letter[0];
-  //   copy = copy.substring(letter.index);
-  // }
-  return output;
-  // return num ? String.repeat() : output;
+export const decode = (message, output = "") => {
+  if (!message.length) return output;
+
+  const num = message.match(/^\d+/);
+  const character = message.match(/[^\d]/);
+  const res = output + character[0].repeat(num ? num[0] : 1);
+
+  return decode(message.substring(character.index + 1), res);
 };
